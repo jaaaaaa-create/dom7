13a4d03976cdc6f51d3e6b4caa878dad9582cf07
ДНС <alym0v.egor@yandex.ru>
2025-08-11 17:58:05 +0300

    dom7

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Author authorBuratino = new Author("Алексей", "Толстой");
+        Book buratino = new Book("Буратино", authorBuratino.getNameAuthor()+" "+authorBuratino.getSurnameAuthor(), 2021);
+        System.out.println("Название книги - "+buratino.getTitle()+ ", автор книги - "+ buratino.getAuthor()+ ", год выпуска - "+ buratino.getYearOfPublication());
+        Author authorDubrovskiy = new Author("Алесандр", "Пушкин");
+        Book dubrovskiy = new Book("Дубровский", authorDubrovskiy.getNameAuthor()+" "+authorDubrovskiy.getSurnameAuthor(), 1833);
+        System.out.println("Название книги - "+dubrovskiy.getTitle()+ ", автор книги - "+ dubrovskiy.getAuthor()+ ", год выпуска - "+ dubrovskiy.getYearOfPublication());
     }
 }
